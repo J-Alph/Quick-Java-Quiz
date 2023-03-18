27526dad28fc810f5069bf80f9a6ff9390630acb
Alphonse Johnson <a.joh26@gmail.com>
2023-03-18 18:21:08 -0500

    Added function names to the javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,66 @@
+var startBtn = document.querySelector("#start-btn");
+
+
+
+
 console.log("hey its working")
+
+var answers = ["variable", "seven", "object", "boolean", "function", "expression"]
+var timer = 60;
+var wins = 0;
+var losses =0;
+var highScore = 0;
+
+
+console.log (answers[2])
+
+
+
+
+// need to start at the beginning, set score to O, set time to 60
+
+function init (){
+    setScore();
+    setTimer();
+
+}
+
+function getScore (){
+   localStorage.getItem("score");
+    
+}
+
+function setSccore (){
+    localStorage.setItem("score");
+    if(highScore === 0){
+        return
+    }else  {
+        highScore = 0;
+    } 
+
+
+}
+
+
+function timeTimer (){
+    
+
+}
+// or run boolean statemnet for else statement
+
+function gameOver (){
+
+}
+
+// target.listner for choices in text area
+
+
+function quizStart (){
+    timeTimer()
+
+}
+
+
+startBtn.addEventListener("click", quizStart)
+
+ init();   
