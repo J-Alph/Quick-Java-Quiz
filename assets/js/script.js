42dbe5554ff1b851f8ab7d6bd0c14f0fba0b9ac1
Alphonse Johnson <a.joh26@gmail.com>
2023-03-18 19:52:33 -0500

    adder timer function. Timer is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,79 @@
-var startBtn = document.querySelector("#start-btn");
-
-
-
-
-console.log("hey its working")
+var startBtn = document.body.querySelector("#start-btn");
+var timeSpan = document.body.querySelector("#zerotime");
 
 var answers = ["variable", "seven", "object", "boolean", "function", "expression"]
-var timer = 60;
+var quizTime = 0;
 var wins = 0;
-var losses =0;
+var losses = 0;
 var highScore = 0;
-
-
-console.log (answers[2])
-
 
 
 
 // need to start at the beginning, set score to O, set time to 60
 
-function init (){
-    setScore();
-    setTimer();
+// function init (){
+//     setScore();
+//     setTimer();
+
+// }
+
+function setTimer() {
+    quizTime = 60;
+}
+
+function getScore() {
+    var myscore = localStorage.getItem("score");
+    if (myscore) {
+
+    }
+
 
 }
 
-function getScore (){
-   localStorage.getItem("score");
-    
-}
-
-function setSccore (){
+function setSccore() {
     localStorage.setItem("score");
-    if(highScore === 0){
+    if (highScore === 0) {
         return
-    }else  {
+    } else {
         highScore = 0;
-    } 
-
-
+    }
 }
 
 
-function timeTimer (){
-    
+function startTimer() {
+
+    var mytimer = setInterval(function () {
+        if (quizTime <= 0) {
+            clearInterval(mytimer)
+            return;
+        };
+        quizTime--;
+        console.log(quizTime);
+
+        timeSpan.textContent =quizTime;
+    }, 1000)
+   
 
 }
 // or run boolean statemnet for else statement
 
-function gameOver (){
+function gameOver() {
 
 }
 
 // target.listner for choices in text area
 
 
-function quizStart (){
-    timeTimer()
-
+function quizStart() {
+    quizTime = 60;
+    startBtn.disabled = true;
+    startTimer()
+    console.log(quizTime) 
 }
 
 
 startBtn.addEventListener("click", quizStart)
 
- init();   
+console.log(quizTime)
+
+//  init();   
